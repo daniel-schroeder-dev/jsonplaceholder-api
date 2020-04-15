746309fcacddf30ea1e9537bbb164bdcef73eac3
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-15 12:30:08 -0500

    Add morgan.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
 
 app.listen(PORT, () => {
   console.log(`jsonplaceholder-api up at: ${PORT}`);
