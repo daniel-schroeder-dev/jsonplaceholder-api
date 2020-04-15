d655fe192db608d7518aa2bcddb64adfe2e9dfde
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-04-15 12:43:31 -0500

    Add dotenv and setup config.

--- src/db/connect.js
+++ src/db/connect.js
@@ -7,10 +7,10 @@ const connectOptions = {
 
 mongoose.set('debug', true);
 
-mongoose.connect('mongodb://localhost:27017/jsonplaceholder-api', connectOptions)
+mongoose.connect(process.env.MONGO_DB_URI, connectOptions)
   .catch(console.error);
 
 mongoose.connection.on('error', console.error);
 mongoose.connection.on('connected', () => {
-  console.log(`Mongoose connected:`);
+  console.log(`Mongoose connected at: ${process.env.MONGO_DB_URI}`);
 });
